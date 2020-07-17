fd063bcd276f42b47d16354f00d6943606444531
apanisileifeoluwa <apanisileifeoluwa@gmail.com>
2020-07-17 11:54:34 +0100

    fied the bug on the login.php page

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your password is: <?php echo $_POST["pas"]; ?>
+Welcome <?php echo $_POST["usrname"]; ?><br>
+Your password is: <?php echo $_POST["psw"]; ?>
 
 </body>
 </html>
